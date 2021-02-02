480e4f0daf2537ad3d635457bd60a151b302212f
SamEdwards2006 <78179452+SamEdwards2006@users.noreply.github.com>
2021-02-02 14:10:23 +1300

    Create MADLIB.py

---
+++ MADLIB.py
@@ -0,0 +1,12 @@
+# Takes the users details
+Number = input("What is your favorite number")
+Subject = input("What is your least favorite subject")
+Age = input("How old are you")
+job = input("What is your dream job")
+transport = input("What is your dream vehicle")
+Age = int(Age)
+# print the story
+print("DEVELOPING STORY")
+print("Breaking news. A", Age+Age, "year old", Subject, "teacher has died in a freak accident involving an out of control",
+transport,"going at", Number, "kilometers an hour.",
+"It was being driven by an intoxicated", job, ". The suspect refuses to talk, mostly due to him being passed out on the floor")
